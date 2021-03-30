c2ea7eca44bbed50195c34849fc09488f2cd2984
JoKing <joebryant614@gmail.com>
2021-03-29 21:55:12 -0700

    optimize and clean up

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+# Password Generator Starter
+
+## Usage
+
+JavaScript
+
+## Processs
+
+This weeks code was given to us and we needed to add the JS to make the page work. For that reason, I only had the one develop branch and worked my code in that branch.
+
+I started with my global arrays that would end up filling my password array "password_content"
+
+I then made 2 main functions. The first fucntion was to make and randomize the password that was given to the user. This function called to my second fucntion which wiith given input from the user used "window.confirm" and an "if" statement to determine what characters would be in the password.
+
+Lastly, I used a "for" loop to fill my password array with a random characater from the array created by the user.
--- script.js
+++ script.js
@@ -2,74 +2,62 @@
 
 // Create Global arrays for project
 var password_content = []
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
-var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var special = ["!","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"] 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 // Create funtion to fill password_content array
 function fillContent() {
 
   var confirmLow = window.confirm("Would you like lower case letters in your password?");
+  var confirmUpper = window.confirm("Would you like upper case letters in your password?");
+  var confirmNumbers = window.confirm("Would you like numbers in your password?");
+  var confirmSpecial = window.confirm("Would you like special characters in your password?");
 
   if (confirmLow) {
     password_content = password_content.concat(lower);
-
-    var confirmUpper = window.confirm("Would you like upper case letters in your password?");
-  }else {
-    var confirmUpper = window.confirm("Would you like upper case letters in your password?");
   }
 
   if (confirmUpper) {
     password_content = password_content.concat(upper);
-
-    var confirmNumbers = window.confirm("Would you like numbers in your password?");
-  }else {
-    var confirmNumbers = window.confirm("Would you like numbers in your password?");
   }
 
   if (confirmNumbers) {
     password_content = password_content.concat(numbers);
-
-    var confirmSpecial = window.confirm("Would you like special characters in your password?");
-  }else {
-    var confirmSpecial = window.confirm("Would you like special characters in your password?");
   }
 
   if (confirmSpecial) {
     password_content = password_content.concat(special);
-  
+  }
   return password_content;
-  }else {
-    return password_content;
-  }
+}
 
-  }
 
 
 // Create function to prompt password length 
 function generatePassword() {
   var password_length = window.prompt("How long would you like your password to be? Please pick a number between 8 and 128.");
 
-if (password_length >= 8 && password_length <= 128) {
+  if (password_length >= 8 && password_length <= 128) {
 
-  fillContent();
-} else {
+    fillContent();
+  } else {
 
-  generatePassword();
-}
-password_length = parseInt(password_length);
+    generatePassword();
+  }
+  password_length = parseInt(password_length);
 
-var passwordFinished = []
+  var passwordFinished = []
 
-for (i=0; i<password_length;i++) {
+  for (i = 0; i < password_length; i++) {
 
     passwordFinished[i] = password_content[Math.floor(Math.random() * password_content.length)]
-}
+  }
 
-passwordFinished = passwordFinished.join("");
+  passwordFinished = passwordFinished.join("");
 
-return passwordFinished
+  return passwordFinished
 
 }
 
